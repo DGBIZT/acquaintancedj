76e514964f8a724ef7221b440be7d9bce27d0b47
DGBIZT <georgiy.gorlov@gmail.com>
2025-07-23 20:06:04 +0300

    изменения в models, в моделе Category, столбец category изменил тип на ForeignKey

--- catalog/models.py
+++ catalog/models.py
@@ -2,11 +2,24 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=150, verbose_name="Название")
+    description = models.TextField(null=True, blank=True)
+    # author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
+
 class Product(models.Model):
     title = models.CharField(max_length=150, verbose_name="Название")
     description = models.TextField(null=True, blank=True)
     image = models.ImageField(upload_to='photos/', verbose_name='Фотография')
-    category = models.CharField(max_length=150, verbose_name="Категория")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     purchase_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
@@ -19,14 +32,3 @@ class Product(models.Model):
         verbose_name_plural = 'Продукты'
         ordering = ['title']
 
-class Category(models.Model):
-    title = models.CharField(max_length=150, verbose_name="Название")
-    description = models.TextField(null=True, blank=True)
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
-        ordering = ['title']
