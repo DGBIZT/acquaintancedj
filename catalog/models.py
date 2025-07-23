18a102a1c14cee7432be8d1f04eb273614c4897c
DGBIZT <georgiy.gorlov@gmail.com>
2025-07-23 18:54:14 +0300

    Второе задание выполнено, создание моделей продукт и категории

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,32 @@
+
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=150, verbose_name="Название")
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='photos/', verbose_name='Фотография')
+    category = models.CharField(max_length=150, verbose_name="Категория")
+    purchase_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ['title']
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, verbose_name="Название")
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
