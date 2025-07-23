a1f2659638b3017fbd91cda1070e27d23bdfbe1c
DGBIZT <georgiy.gorlov@gmail.com>
2025-07-23 21:11:53 +0300

    выполнено задание 4: Создание суперпользователя, регистрацияч моделей Product, Category настроена отображение моделей

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Category, Product
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_filter = ('title',)
+    search_fields = ('title', 'id')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'purchase_price', 'category',)
+    list_filter = ('category',)
+    search_fields = ('title', 'description')
