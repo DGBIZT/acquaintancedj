60c5d8593ddc078c8c82c7558f3f786af8d4aa50
DGBIZT <georgiy.gorlov@gmail.com>
2025-07-23 21:20:04 +0300

    Добавление в models.py в модкли Product в столбце category verbose_name=Категория

--- catalog/models.py
+++ catalog/models.py
@@ -19,7 +19,7 @@ class Product(models.Model):
     title = models.CharField(max_length=150, verbose_name="Название")
     description = models.TextField(null=True, blank=True)
     image = models.ImageField(upload_to='photos/', verbose_name='Фотография')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name='Категория')
     purchase_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
