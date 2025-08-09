139acb2f774900a0e848fd33d91cf856007b1f83
DGBIZT <georgiy.gorlov@gmail.com>
2025-08-09 19:49:30 +0300

    Выполнено первое задание FBV и CBV, переведены имеющиеся в проекте контроллеры с FBV на CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,15 +3,24 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import SubmitDataView, ProductListView, ProductDetailView, ProductTemplateView
+
 app_name = "catalog"
 
 urlpatterns = ([
-    path('home/', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts'),
-    path('submit_data/', views.submit_data, name='submit_data'),
-    path('productinform/', views.product_information, name='product_information'),
-    path('index/', views.index, name='index'),
-    path('product_detail/<int:product_id>', views.product_detail, name='product_detail'),
-    path('base/', views.base, name='base'),
+    path('home/', ProductListView.as_view(), name='home'),
+    path('contacts/', ProductTemplateView.as_view(), name='contacts'),
+    path('submit_data/', SubmitDataView.as_view(), name='submit_data'),
+    path('product_detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+
 ] )
 
+# urlpatterns = ([
+#     path('home/', views.home, name='home'),
+#     path('contacts/', views.contacts, name='contacts'),
+#     path('submit_data/', views.submit_data, name='submit_data'),
+#     path('productinform/', views.product_information, name='product_information'),
+#     path('index/', views.index, name='index'),
+#     path('product_detail/<int:product_id>', views.product_detail, name='product_detail'),
+#     path('base/', views.base, name='base'),
+# ] )
--- catalog/views.py
+++ catalog/views.py
@@ -1,57 +1,79 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import View, ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
 from catalog.models import Product
 
 
 # Create your views here.
+class SubmitDataView(View): # SubmitDataView — это гибкий инструмент для обработки HTTP-запросов в Django
 
-def submit_data(request):
-    if request.method == "POST":
+    def post(self): # def post(self, request, *args, **kwargs):
         return HttpResponse("Данные отправлены")
 
-def home(request):
-    products = Product.objects.all()
-    context = {'products': products}
-    return render(request, "catalog/home.html", context=context)
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/home.html'
+    context_object_name = 'products'
 
-def contacts(request):
-    return render(request, "catalog/contacts.html")
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product_detail.html'
+    context_object_name = 'product'
 
-def product_information(request):
-    product = Product.objects.get(id=3)
-    context = {
-        "product_name": f'{product.title}',
-        'product_description': f'{product.description}',
-        'product_image': product.image , # передаем само поле image - product.image, а не его строковое представление f'{product.image}'
-    }
-    return render(request, "catalog/productinform.html", context=context)
+class ProductTemplateView(TemplateView):
+    model = Product
+    template_name = 'catalog/contacts.html'
+    context_object_name = 'contacts'
 
 
-def product_detail(request, product_id):
-    try:
-        product = Product.objects.get(id=product_id)
 
-        if not product.image:
-            print("Изображение не загружено")
-        elif not product.image.storage.exists(product.image.name):
-            print(f"Файл не существует по пути: {product.image.path}")
-        print(f"Путь в БД: {product.image.name}")  # Должно быть photos/Buckwheat.jpg
-        print(f"URL: {product.image.url}")  # Должно быть /media/photos/Buckwheat.jpg
-        print(f"Физический путь: {product.image.path}")  # Полный путь к файлу
-        print(f"Полный путь к файлу: {product.image.path}")
-    except Product.DoesNotExist:
-        product = None
-    context = {
-        "product": product,
-    }
-    return render(request, 'catalog/product_detail.html', context)
-
-
-def index(request):
-    return render(request, "catalog/index.html")
-
-
-def base(request):
-    products = Product.objects.all()
-    context = {'products': products}
-    return render(request, 'catalog/base.html', context=context)
+# def submit_data(request):
+#     if request.method == "POST":
+#         return HttpResponse("Данные отправлены")
+#
+# def home(request):
+#     products = Product.objects.all()
+#     context = {'products': products}
+#     return render(request, "catalog/home.html", context=context)
+#
+# def contacts(request):
+#     return render(request, "catalog/contacts.html")
+#
+# def product_information(request):
+#     product = Product.objects.get(id=3)
+#     context = {
+#         "product_name": f'{product.title}',
+#         'product_description': f'{product.description}',
+#         'product_image': product.image , # передаем само поле image - product.image, а не его строковое представление f'{product.image}'
+#     }
+#     return render(request, "catalog/productinform.html", context=context)
+#
+#
+# def product_detail(request, product_id):
+#     try:
+#         product = Product.objects.get(id=product_id)
+#
+#         if not product.image:
+#             print("Изображение не загружено")
+#         elif not product.image.storage.exists(product.image.name):
+#             print(f"Файл не существует по пути: {product.image.path}")
+#         print(f"Путь в БД: {product.image.name}")  # Должно быть photos/Buckwheat.jpg
+#         print(f"URL: {product.image.url}")  # Должно быть /media/photos/Buckwheat.jpg
+#         print(f"Физический путь: {product.image.path}")  # Полный путь к файлу
+#         print(f"Полный путь к файлу: {product.image.path}")
+#     except Product.DoesNotExist:
+#         product = None
+#     context = {
+#         "product": product,
+#     }
+#     return render(request, 'catalog/product_detail.html', context)
+#
+#
+# def index(request):
+#     return render(request, "catalog/index.html")
+#
+#
+# def base(request):
+#     products = Product.objects.all()
+#     context = {'products': products}
+#     return render(request, 'catalog/base.html', context=context)
