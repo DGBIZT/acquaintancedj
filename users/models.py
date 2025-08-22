ae5ef5c71836889d62d03bd67fb4fb40cd9c1515
George Gorlov <Georgiy.Gorlov@gmail.com>
2025-08-22 11:52:36 +0300

    Создал новое приложение users, определил пользователя с использованием ABstructUser, задав электронную почту как поле для авторизации, добавил доп поля

---
+++ users/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    countries = models.CharField(max_length=65, blank=True, null=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username',]
+
+    def __str__(self):
+        return self.email
+
+
