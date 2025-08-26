6862ca0542f2c7e9917d2f53fda38462346126e5
DGBIZT <georgiy.gorlov@gmail.com>
2025-08-26 20:07:57 +0300

    Добавил право в группу модератор can_publish_product

--- users/management/commands/create_groups.py
+++ users/management/commands/create_groups.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import Group, Permission
 class Command(BaseCommand):
     def handle(self, *args, **options):
         group, created = Group.objects.get_or_create(name='Модератор продуктов')
-        permissions = Permission.objects.filter(codename__in=['can_unpublish_product', 'delete_product', 'view_product', 'change_product', 'add_product', 'delete_blog', 'view_blog', 'change_blog', 'add_blog'])
+        permissions = Permission.objects.filter(codename__in=['can_unpublish_product','can_publish_product',  'delete_product', 'view_product', 'change_product', 'add_product', 'delete_blog', 'view_blog', 'change_blog', 'add_blog'])
         group.permissions.set(permissions)
         self.stdout.write(self.style.SUCCESS('Группа "Модератор продуктов" создана с необходимыми правами.'))
 
